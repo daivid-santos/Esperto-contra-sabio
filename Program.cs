cfcf8ae4fdc55ddab65d347ec47ebda95ab72102
Daivid <daivid.santos00989@gmail.com>
2025-12-05 19:57:03 -0300

    Exercicio Finalizado

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿Console.Clear();
+
+
+
+Console.WriteLine("--- Esperto Contra Sabido ---\n");
+
+Console.ForegroundColor = ConsoleColor.Blue;
+
+Console.Write("Quantos alimentos serão distribuídos? ");
+int total = Convert.ToInt32(Console.ReadLine());
+
+if (total <= 0) return;
+
+Console.WriteLine();
+
+int restantes = total;
+int picaPau = 0, raposinha = 0;
+
+int contadorParaVoce = 0, contadorParaMim = 0;
+while (restantes > 0)
+{
+    contadorParaVoce++;
+    picaPau += 1;
+    restantes -= 1;
+Console.ForegroundColor = ConsoleColor.Magenta;
+    Console.Write($"{picaPau} para você. ");
+
+    contadorParaMim = 0;
+    while (restantes > 0 && contadorParaMim < contadorParaVoce)
+    {
+        contadorParaMim++;
+        raposinha += 1;
+        restantes -= 1;
+
+        Console.Write($"{contadorParaMim}");
+
+        if (restantes > 0 && contadorParaMim < contadorParaVoce)
+        {
+            Console.Write(",");
+        }
+
+        Console.Write(" ");
+    }
+
+    if (contadorParaMim > 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine("para mim.");
+    }
+}
+
+if (contadorParaMim == 0)
+{
+    Console.WriteLine();
+}
+
+Console.ForegroundColor = ConsoleColor.Green;
+
+Console.WriteLine();
+Console.WriteLine($"Pica-pau recebeu {picaPau} alimento(s).");
+Console.WriteLine($"Raposinha recebeu {raposinha} alimento(s).");
+
+Console.ResetColor();
